d419214bc7be68ea85bccc092a2401c3eb207eae
Anand Vimalanathan <tomeanand@gmail.com>
2014-01-05 01:02:14 +0530

    - Services added
     - added menu page and incorporated in list

--- app/app.js
+++ app/app.js
@@ -2,13 +2,19 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('spiceupApp', 
-	['Centralway.lungo-angular-bridge']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+var spiceApp = angular.module('spiceupApp', 
+	['Centralway.lungo-angular-bridge'
+	,'spiceUpControllers'
+	,'spiceUpServices']
+	);
+
+  spiceApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
     $routeProvider.when('/dynamic', {templateUrl: "app/partials/dynamic.html", controller: "DynamicCtrl"});
     $routeProvider.when('/deeplink/view/:msg', {templateUrl: "app/partials/deeplink.html", controller: "DeeplinkCtrl"});
     $routeProvider.when('/static', {templateUrl: "app/partials/static.html"});
     $routeProvider.when("/static/info", {});
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(false);
+
   }]);  
--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,14 +1,35 @@
-function AppCtrl($scope, $location) {    
-  $scope.triggerAside = function() {
-    console.log('triggering aside');
-    Lungo.Router.aside('main', 'aside1');
-  }    
-}
+'use strict';
 
-function DynamicCtrl($scope) {
-    $scope.message = "Hello world!";
-}
+/* Controllers */
 
-function DeeplinkCtrl($scope, $routeParams) {
-    $scope.paramMsg = $routeParams.msg;
-}
+var spiceUpControllers = angular.module('spiceUpControllers', []);
+
+spiceUpControllers.run(function()	{
+	console.log('Hello there');
+
+})
+
+spiceUpControllers.controller('AppCtrl',['$scope', '$location','MenuResponder',
+	function($scope, $location, MenuResponder)	{
+		$scope.$parent.$root['manuList'] = MenuResponder.query();
+		
+		$scope.triggerAside = function() {
+			console.log('triggering aside');
+			Lungo.Router.aside('main', 'aside1');
+		}
+	}
+	]);
+
+spiceUpControllers.controller ('DynamicCtrl',['$scope', '$location','MenuResponder',
+	function($scope, $location, MenuResponder)	{
+
+		$scope.menuData = $scope.$parent.$root['manuList']
+		$scope.message = "Hello world!";
+
+	}]);
+
+spiceUpControllers.controller ('DeeplinkCtrl',['$scope', '$routeParams',
+	function($scope, $routeParams)	{
+		$scope.paramMsg = $routeParams.msg;
+	}]);
+
