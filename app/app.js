64ecc15a76dab600980006a51ddc42a68d3bb4d7
manjaligirimxbit13 <manjaligm87@gmail.com>
2014-01-16 11:38:00 +0530

    screen creations two pages

--- app/app.js
+++ app/app.js
@@ -9,11 +9,14 @@ var spiceApp = angular.module('spiceupApp',
 	);
 
   spiceApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-
-    $routeProvider.when('/dynamic', {templateUrl: "app/partials/dynamic.html", controller: "DynamicCtrl"});
+ 
+    $routeProvider.when('/basemenu', {templateUrl: "app/partials/basemenu.html", controller: "DynamicCtrl"});
     $routeProvider.when('/deeplink/view/:msg', {templateUrl: "app/partials/deeplink.html", controller: "DeeplinkCtrl"});
     $routeProvider.when('/static', {templateUrl: "app/partials/static.html"});
     $routeProvider.when("/static/info", {});
+    $routeProvider.when("/menudetail", {templateUrl: "app/partials/menudetail.html"});
+    $routeProvider.when('/homepage', {templateUrl: "app/partials/homepage.html"});
+     $routeProvider.when('/subcategorylist', {templateUrl: "app/partials/subcategorylist.html", controller: "DynamicCtrl"});
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(false);
 
