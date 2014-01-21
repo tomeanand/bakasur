1bfc41f076563b6fda896787410cf58ef4d73ed9
manjaligirimxbit13 <manjaligm87@gmail.com>
2014-01-21 17:07:18 +0530

    screen creations pages- superuser reservationlist,detail

--- app/app.js
+++ app/app.js
@@ -12,18 +12,19 @@ var spiceApp = angular.module('spiceupApp',
  
     $routeProvider.when('/superuser', {templateUrl: "app/partials/superuser.html"});
     $routeProvider.when('/adminreservation', {templateUrl: "app/partials/adminreservation.html"});
-    $routeProvider.when('/homepage/list', {templateUrl: "app/partials/homepage.html", controller: "DynamicCtrl"});
+    $routeProvider.when('/adminreservation/reservationdetail', {templateUrl: "app/partials/adminreservation.html"});
+    $routeProvider.when('/catlist', {templateUrl: "app/partials/catlist.html", controller: "DynamicCtrl"});
     $routeProvider.when('/deeplink/view/:msg', {templateUrl: "app/partials/deeplink.html", controller: "DeeplinkCtrl"});
     $routeProvider.when('/static', {templateUrl: "app/partials/static.html"});
     $routeProvider.when("/static/info", {});
     $routeProvider.when("/menudetail", {templateUrl: "app/partials/menudetail.html"});
-    $routeProvider.when('/homepage', {templateUrl: "app/partials/homepage.html"});
+    $routeProvider.when('/homepage/home', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/homepage/myorder', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/homepage/form-normal', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/homepage/contact', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/detail', {templateUrl: "app/partials/detail.html"});
     $routeProvider.when('/menus', {templateUrl: "app/partials/menu.html"});
-    $routeProvider.otherwise({redirectTo: '/homepage'});
+    $routeProvider.otherwise({redirectTo: '/homepage/home'});
     $locationProvider.html5Mode(false);
 
   }]);  
