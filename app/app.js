e428a2d20c6725e34da88f7953452bad72e30728
manjaligirimxbit13 <manjaligm87@gmail.com>
2014-01-22 17:21:00 +0530

    navigation done in showall,menu,menudetail

--- app/app.js
+++ app/app.js
@@ -13,18 +13,22 @@ var spiceApp = angular.module('spiceupApp',
     $routeProvider.when('/superuser', {templateUrl: "app/partials/superuser.html"});
     $routeProvider.when('/adminreservation', {templateUrl: "app/partials/adminreservation.html"});
     $routeProvider.when('/adminreservation/reservationdetail', {templateUrl: "app/partials/adminreservation.html"});
-    $routeProvider.when('/catlist', {templateUrl: "app/partials/catlist.html", controller: "DynamicCtrl"});
+   // $routeProvider.when('/catlist', {templateUrl: "app/partials/catlist.html", controller: "DynamicCtrl"});
+    $routeProvider.when('/homepage/showallmenu', {templateUrl: "app/partials/homepage.html"});
+    $routeProvider.when('/homepage/menus', {templateUrl: "app/partials/homepage.html", controller: "DynamicCtrl"});
+    $routeProvider.when('/homepage/menuitems', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/deeplink/view/:msg', {templateUrl: "app/partials/deeplink.html", controller: "DeeplinkCtrl"});
     $routeProvider.when('/static', {templateUrl: "app/partials/static.html"});
     $routeProvider.when("/static/info", {});
-    $routeProvider.when("/menudetail", {templateUrl: "app/partials/menudetail.html"});
+    $routeProvider.when("/homepage/menudetail", {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/homepage/home', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/homepage/myorder', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/homepage/form-normal', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/homepage/contact', {templateUrl: "app/partials/homepage.html"});
-    $routeProvider.when('/detail', {templateUrl: "app/partials/detail.html"});
+    $routeProvider.when('/homepage/login', {templateUrl: "app/partials/homepage.html"});
     $routeProvider.when('/menus', {templateUrl: "app/partials/menu.html"});
-    $routeProvider.otherwise({redirectTo: '/homepage/home'});
+    $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(false);
 
   }]);  
+
