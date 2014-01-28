b0f4989f7b0c8b4f86942d66f7bc47692a63a00c
manjaligirimxbit13 <manjaligm87@gmail.com>
2014-01-28 11:48:10 +0530

    screen creations-superuser

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -9,10 +9,12 @@ spiceUpControllers.run(function()	{
 
 })
 
-spiceUpControllers.controller('AppCtrl',['$scope', '$location','MenuResponder','MenuService','menuBycat',
-	function($scope, $location, MenuResponder,MenuService,menuBycat)	{
+spiceUpControllers.controller('AppCtrl',['$scope', '$location','MenuResponder',
+								'MenuService','menuBycat','menuIdPost',
+	function($scope, $location, MenuResponder,MenuService,menuBycat,menuIdPost)	{
 		$scope.$parent.$root['manuList'] = MenuResponder.query();
 		$scope.$parent.$root['menuList'] = MenuService.query();
+		$scope.$parent.$root['IdPost'] = menuIdPost.query();
 		$scope.$parent.$root['CatMenu'] = menuBycat.query({menu_category : 1});
 
 		$scope.triggerAside = function() {
@@ -39,12 +41,28 @@ spiceUpControllers.controller ('menuCtrl',['$scope', '$location','MenuService','
 	function($scope, $location, $routeParams,MenuService,menuBycat)	{
 		$scope.Showmenu = $scope.$parent.$root['menuList']
 
-		$scope.ShowmenuByCat = 	$scope.$parent.$root['CatMenu'] 
+		$scope.ShowmenuByCat= 	$scope.$parent.$root['CatMenu'] 
 		$scope.paramMsg = $routeParams.menu_category;
 
 	}]);
 
 
+spiceUpControllers.controller('PostCtrl', ['$scope','$location', '$routeParams', 'menuIdPost',
+  function($scope,$location, $routeParams, menuIdPost) {
+
+  	$scope.ShowmenuById = 	$scope.$parent.$root['IdPost'] 
+    // $scope.post = menuIdPost.get({menu_id: $routeParams.menu_id});
+$scope.post =$routeParams.menu_id;
+$scope.post =$routeParams.menu_name;
+  }]);
+
+
+// angular.module('clientApp')
+//   .controller('ProductCtrl', function ($scope, ProductsFactory, $modal, $log, $stateParams) {
+//     var productItems = ProductsFactory.getProducts()
+//     $scope.products = productItems.query();
+//     $scope.selectedProduct = productItems.get({productId:1});
+//   });
 // spiceUpControllers.controller ('CatCtrl',['$scope', '$location','menuBycat',
 // 	function($scope, $location,menuBycat)	{
 		 
